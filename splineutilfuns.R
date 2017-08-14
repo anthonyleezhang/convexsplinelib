745dd2398652df051c763b5f8f6ef1cd73dd51e1
Anthony Lee Zhang <anthonyz@stanford.edu>
2017-08-14 12:12:29 -0700

    Adding testcode and cspline_approx

--- splineutilfuns.R
+++ splineutilfuns.R
@@ -17,3 +17,22 @@ make_splinemat = function(xvec, splinelist) {
   }
   return(splinemat)
 }
+
+cspline_approx = function(splinemat, targetvec) {
+  obj = function(paramvec) {
+    predvec = splinemat %*% paramvec
+    return(sum((targetvec - predvec)^2))
+  }
+  
+  out = optim(rep(0, length(splinelist)),
+              obj, 
+              lower = c(rep(0, length(splinelist) - 2), -Inf, -Inf),
+              method = "L-BFGS-B")
+  paramvec = out$par
+  predvec = splinemat %*% paramvec
+  
+  outlist = list(param = paramvec, predvec = predvec)
+}
+
+
+
--- testcode/testcode.R
+++ testcode/testcode.R
@@ -1,37 +1,68 @@
-setwd("E:/Dropbox/projects/convexsplinelib")
-
 source("convexsplines.R")
 source("convexspline_derivs.R")
+source("splineutilfuns.R")
 
-knotvec = c(1,2.5,3,4)
+## Make splines
+
+knotvec = c(1,2.5,3,5,6)
 
 splinelist = make_csplines(knotvec)
 
-xvec = seq(0, 5, length.out = 1000)
+xvec = seq(0, 6, length.out = 1000)
+splinemat = make_splinemat(xvec, splinelist)
 
-eval_splinemat = function(theta, splinemat) {
-  return(splinemat %*% theta)
-}
+data = data.table(splinemat)
+setnames(data, c("y1", "y2", "y3", "y4", "y5", "y6", "y7"))
+data[, x := xvec]
 
-eval_spline = function(theta, xvec, splinelist) {
-  out = rep(0, length(xvec))
-  for(i in 1:length(splinelist)) {
-    out = out + theta[i] * splinelist[[i]](xvec)
-  }
-  return(out)
-}
+## Plot splines
 
-make_splinemat = function(xvec, splinelist) {
-  splinemat = numeric(0)
-  for(i in 1:length(splinelist)) {
-    splinemat = cbind(splinemat, splinelist[[i]](xvec))
-  }
-  return(splinemat)
-}
+plotdata = melt(data, id.vars = "x")
 
-asdf = data.table(splinemat)
-asdf[, index := xvec]
+ggplot(plotdata, aes(x = x, y = value, group = variable, color = variable)) + 
+  geom_line(size = 1.3)
 
-slp(asdf, index, V6)
+## Approximate some functions
 
-data = data.table(x = seq(0, 5, length.out = 1000))
+data[, expfun := exp(xvec)]
+out = cspline_approx(splinemat, data$expfun)
+data[, pred_expfun := out$predvec]
+
+plotdata = rbindlist(list(
+  data[, .(x = xvec, y = expfun, Series = "Exponential")],
+  data[, .(x = xvec, y = pred_expfun, Series = "Spline Approx")]
+))
+
+ggplot(plotdata, aes(x = x, y = y, group = Series, color = Series)) + 
+  geom_line(size = 1.3)
+
+# Wavy exponential
+
+data[, wavyfun := exp(xvec) + 50 * sin(5 * xvec)]
+out = cspline_approx(splinemat, data$wavyfun)
+data[, pred_wavyfun := out$predvec]
+
+plotdata = rbindlist(list(
+  data[, .(x = xvec, y = wavyfun, Series = "Wavy exponential")],
+  data[, .(x = xvec, y = pred_wavyfun, Series = "Spline Approx")]
+))
+
+ggplot(plotdata, aes(x = x, y = y, group = Series, color = Series)) + 
+  geom_line(size = 1.3)
+
+## Statistical approximation
+# Noisy exponential
+
+data[, noisyexp := exp(xvec) + rnorm(.N, sd = 30)]
+out = cspline_approx(splinemat, data$noisyexp)
+data[, pred_noisyexp := out$predvec]
+
+ggplot(data) + 
+  geom_point(aes(x = xvec, y = noisyexp), size = 2, alpha = 0.5) + 
+  geom_line(aes(x = xvec, y = pred_noisyexp), size = 1.3, color = "blue")
+  
+
+
+
+
+
